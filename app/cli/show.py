dd3c1c60fbb5ab245725b5c4ef633940948056dc
Berat Çimen <beratcmn@hotmail.com>
2024-03-30 14:09:54 +0300

    Added show commadn

--- app/cli/show.py
+++ app/cli/show.py
@@ -19,5 +19,26 @@ def global_venv_path():
         typer.echo(f"Global virtual environment path: {DEFAULT_VENV_PATH}")
 
 
+@app.command()
+def venvs():
+    """Lists all the virtual environments."""
+
+    if CONFIG_FILE.exists():
+        with open(CONFIG_FILE, "r", encoding="UTF-8") as f:
+            config = json.load(f)
+            venv_path = Path(config["venv_path"])
+    else:
+        venv_path = DEFAULT_VENV_PATH
+
+    if not venv_path.exists():
+        typer.echo("No virtual environments found.")
+        raise typer.Exit()
+
+    typer.echo("Virtual environments:")
+    for venv in venv_path.iterdir():
+        if venv.is_dir():
+            typer.echo(f" - {venv.name}")
+
+
 if __name__ == "__main__":
     app()
